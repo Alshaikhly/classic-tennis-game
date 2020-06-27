82c256dccda5137ad9b3552a2e8c1257dea520fb
Ali Al Shaikhly <alshaikhly89@gmail.com>
2020-06-27 01:15:24 -0600

    add second player with simple AI

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const canvas = document.getElementById('gameCanvas');
 const canvasContext = canvas.getContext('2d');
 let ballX = 200;
-let ballSpeedX = 15;
+let ballY = 200;
+let ballSpeedX = 10;
+let ballSpeedY = 10;
+let paddle1Y = 250;
+let paddle2Y = 250;
+const paddleThickness = 10;
+const paddleHeight = 100;
+
+let player1Score = 0;
+let player2Score = 0;
 
 // colorRect is a function that takes the 5 parameters to draw a rect on canvas
 const colorRect = (leftX, topY, width, height, drawColor) => {
@@ -15,33 +24,97 @@ const colorCirc = (centerX, centerY, radius, drawColor) => {
   canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
   canvasContext.fill();
 }
+
+// function to reset the ball in the ventre of canvas
+
+const ballReset = () => {
+  ballSpeedX= -ballSpeedX;
+  ballX = canvas.width/2;
+  ballY = canvas.height/2;
+}
+
+// function to track mouse movement on the canvas
+
+const calculateMousePos = evt => {
+  let rect = canvas.getBoundingClientRect();
+  let root = document.documentElement;
+  let mouseX = evt.clientX - rect.left - root.scrollLeft;
+  let mouseY = evt.clientY - rect.top - root.scrollTop;
+
+  return {
+    x: mouseX,
+    y: mouseY
+  };
+};
+
+const computerMovement = () => {
+  const paddle2YCenter = paddle2Y + paddleHeight/2;
+  if (paddle2YCenter < ballY -35) {
+    paddle2Y += 10;
+  } else if (paddle2YCenter > ballY +35) {
+    paddle2Y -= 10;
+  }
+}
 // creates the rectangles needed for the game
 const gameArea = () => {
-  colorRect(0, 0, canvas.width, canvas.height, 'black')
-  colorRect(0,210,10,100, 'white');
+  // drawing the canvas
+  colorRect(0, 0, canvas.width, canvas.height, 'black');
+
+  // drawing the paddles
+  colorRect(0,paddle1Y, paddleThickness, paddleHeight, 'white');
+  colorRect(canvas.width - paddleThickness, paddle2Y, paddleThickness, paddleHeight, 'white');
 
   //drawing of the ball
-  
-  colorCirc(ballX,150,10, 'yellow');
+  colorCirc(ballX, ballY, 10, 'yellow');
+
+  // add score to the screen
+  canvasContext.fillText(player1Score, 100, 100);
+  canvasContext.fillText(player2Score, canvas.width - 100, 100);
 }
 
 // controls the movement of the ball
 const movement = () => {
   ballX += ballSpeedX;
-  if (ballX < 0) {
-    ballSpeedX= -ballSpeedX;
+  ballY += ballSpeedY;
+  if (ballX < paddleThickness) {
+    if (ballY > paddle1Y && ballY < paddle1Y + paddleHeight) {
+      ballSpeedX= -ballSpeedX;
+    } else {
+      ballReset();
+      player2Score++;
+    }
   }
-  if (ballX > canvas.width) {
-    ballSpeedX = -ballSpeedX;
+  if (ballX > canvas.width - paddleThickness) {
+    if (ballY > paddle2Y && ballY < paddle2Y + paddleHeight) {
+      ballSpeedX= -ballSpeedX;
+    } else {
+      ballReset();
+      player1Score++;
+    }
+  }
+  if (ballY < 0) {
+    ballSpeedY= -ballSpeedY;
+  }
+  if (ballY > canvas.height) {
+    ballSpeedY = -ballSpeedY;
   }
 }
 
 const callBoth = () => {
-  gameArea()
-  movement()
-}
+  gameArea();
+  movement();
+  computerMovement();
+};
+
 $(document).ready(function() {
   
   const framesPerSecond = 30;
   setInterval(callBoth, 1000/framesPerSecond);
+
+  canvas.addEventListener('mousemove', 
+    function(evt) {
+      let mousePos = calculateMousePos(evt);
+      paddle1Y = mousePos.y - paddleHeight/2;
+    });
+
 })
