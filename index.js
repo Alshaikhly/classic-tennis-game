06a68b890d4b0db5402f4a761d38060116d32a29
Ali Al Shaikhly <alshaikhly89@gmail.com>
2020-07-03 01:07:05 -0600

    Add winning score and game restart click

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const canvas = document.getElementById('gameCanvas');
 const canvasContext = canvas.getContext('2d');
-let ballX = 250;
-let ballY = 250;
-let ballSpeedX = 10;
-let ballSpeedY = 4;
+let ballX = 250; // location of the ball in px on the x-axis over the canvas element(counting from the left edge of canvas)
+let ballY = 250; // same as X but on the y-axis
+let ballSpeedX = 10; // the ball moves 10 px on the x-axis with every time interval >> setInterval.
+let ballSpeedY = 4; // same as X on the y-axis
 let paddle1Y = 250;
 let paddle2Y = 250;
 const paddleThickness = 10;
 const paddleHeight = 100;
 let player1Score = 0;
 let player2Score = 0;
+const winningScore = 2;
+let showingWinScreen = false;
 
 // colorRect is a function that takes the 5 parameters to draw a rect on canvas
 const colorRect = (leftX, topY, width, height, drawColor) => {
@@ -24,9 +26,17 @@ const colorCirc = (centerX, centerY, radius, drawColor) => {
   canvasContext.fill();
 }
 
+const drawNet = () => {
+  for (let i = 0; i <canvas.height; i+=40) {
+    colorRect(canvas.width/2-1, i,2,20,'white')
+  }
+}
 // function to reset the ball in the ventre of canvas
 
 const ballReset = () => {
+  if (player1Score >= winningScore || player2Score >= winningScore) { // check if winning condition is reached then do this to reset the game
+    showingWinScreen = true;
+  }
   ballSpeedX = - ballSpeedX;
   ballX = canvas.width/2;
   ballY = canvas.height/2;
@@ -59,6 +69,18 @@ const gameArea = () => {
   // drawing the canvas
   colorRect(0, 0, canvas.width, canvas.height, 'black');
 
+  if (showingWinScreen) {
+    canvasContext.fillStyle = 'white';
+    if (player1Score >= winningScore) {
+      canvasContext.fillText("🎾🎾🎾Player 1 won!!🎾🎾🎾", 300, 200);
+    } else if (player2Score >= winningScore) {
+      canvasContext.fillText("🎾🎾🎾Computer won!!🎾🎾🎾", 300, 200);
+    }
+    canvasContext.fillText("click anywhere to play again", 320, 450);
+    return; // this is just to bail out of the function early
+  }
+
+  drawNet()
   // drawing the paddles
   colorRect(0,paddle1Y, paddleThickness, paddleHeight, 'white');
   colorRect(canvas.width - paddleThickness, paddle2Y, paddleThickness, paddleHeight, 'white');
@@ -73,6 +95,9 @@ const gameArea = () => {
 
 // controls the movement of the ball
 const movement = () => {
+  if (showingWinScreen) {
+    return; // this is just to bail out of the function early
+  }
   ballX += ballSpeedX;
   ballY += ballSpeedY;
   if (ballX < paddleThickness) {
@@ -81,8 +106,8 @@ const movement = () => {
       let deltaY1 = ballY - (paddle1Y + paddleHeight/2)
       ballSpeedY = deltaY1 * 0.35
     } else {
+      player2Score++; // MUST be before ballreset to check if the score needs to end the game
       ballReset();
-      player2Score++;
     }
   }
   if (ballX > canvas.width - paddleThickness) {
@@ -91,8 +116,8 @@ const movement = () => {
       let deltaY2 = ballY - (paddle2Y + paddleHeight/2)
       ballSpeedY = deltaY2 * 0.35
     } else {
-      ballReset();
       player1Score++;
+      ballReset(); 
     }
   }
   if (ballY < 0) {
@@ -109,11 +134,20 @@ const callBoth = () => {
   computerMovement();
 };
 
+const handleMouseClick = evt => {
+  if (showingWinScreen) {
+    player1Score = 0;
+    player2Score = 0;
+    showingWinScreen = false
+  }
+}
 $(document).ready(function() {
   
   const framesPerSecond = 30;
   setInterval(callBoth, 1000/framesPerSecond);
 
+  canvas.addEventListener('mousedown', handleMouseClick);
+  
   canvas.addEventListener('mousemove', 
     function(evt) {
       let mousePos = calculateMousePos(evt);
